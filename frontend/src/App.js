35b20bd54b2895f716f90ba4ba67b77d8ddb0123
Anusha-Patil05 <patilanusha34@gmail.com>
2026-03-29 18:52:58 +0530

    fixed frontend tracking

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Cognitive Debugger</h1>
+      <button>Start</button>
+      <button>Stop</button>
+      <p>Time: 0</p>
+    </div>
+  );
+}
+
+export default App;
