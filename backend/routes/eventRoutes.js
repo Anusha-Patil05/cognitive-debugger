4fc0994c5d1c0afbbc86b8a4db3739c23264d224
Anusha-Patil05 <patilanusha34@gmail.com>
2026-03-30 19:34:19 +0530

    Resolve merge conflict in event routes

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,17 +1,7 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-
-require.post("/event", (req, res) => {
-    console.log("Received event:", req.body);
-    res.send("ok");
-})
-
-module.exports = router;
-=======
 const { handleEvent } = require("../controllers/eventController");
 
 router.post("/event", handleEvent);
 
 module.exports = router;
->>>>>>> feature/backend-receiver
